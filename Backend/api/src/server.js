f58547ef81e4ed4fd40469742d4c1784b1edc142
andre.zambroni <andre.zambroni@accurate.com.br>
2024-08-31 15:55:55 -0300

    https

--- Backend/api/src/server.js
+++ Backend/api/src/server.js
@@ -1,12 +1,22 @@
-import http from "node:http" // Importa o módulo HTTP nativo do Node.js
+import http from "node:http"; // Importa o módulo HTTP nativo do Node.js
 
 // Cria um servidor HTTP
 // request representa a requisição recebida pelo servidor.
 // response é o objeto que permite enviar uma resposta de volta ao cliente.
 // response.end("Hello world!") envia a string "Hello world!" como resposta e encerra a interação.
 const server = http.createServer((request, response) => {
+  const { method, url } = request;
+
+  if (method === "GET" && url === "/products") {
+    return response.end("Lista de produtos");
+  }
+
+  if (method === "POST" && url === "/products") {
+    return response.writeHead(201).end("Produto cadastrado");
+  }
+
   // Quando uma requisição é recebida, executa a função callback
-  return response.end("Hello sdasddas!"); // Envia a resposta "Hello world!" e encerra a conexão
+  return response.writeHead(404).end("Rota nao encontrada"); 
 });
 
 // O servidor fica ouvindo por conexões na porta 3333
